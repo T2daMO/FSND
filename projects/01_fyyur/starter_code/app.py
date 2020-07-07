2885504bb0e873f9f8edfcdd6cc8a8058323703b
unknown <tmoiloanyane@gmail.com>
2020-07-07 23:26:01 +0200

    resubmit

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -145,30 +145,14 @@ def show_venue(venue_id):
     venue = Venue.query.all()
     artists = Artist.query.all()
     data1={
-    "id": venue.artist_id,
-      "name": venue.name,
+    "id": "Venue.id",
+      "name": artists.name,
       "genres": venue.genres,
       "address": venue.address,
       "city": venue.city,
-      "state": venue.state,
-      "phone": venue.phone,
-      "website": "https://www.themusicalhop.com",
-      "facebook_link": "https://www.facebook.com/TheMusicalHop",
-      "seeking_talent": True,
-      "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-      "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-      "past_shows": [{
-        "artist_id": 4,
-        "artist_name": "Guns N Petals",
-        "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-        "start_time": "2019-05-21T21:30:00.000Z"
-      }],
-      "upcoming_shows": [],
-      "past_shows_count": 1,
-      "upcoming_shows_count": 0,
-    }
-    
-    }
+      "state": venue.state
+      }
+      
     data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
     return render_template('pages/show_venue.html', venue=data)
 
